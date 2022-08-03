c439c66198c5d2c80476a7bc014b1ab58afaf058
nicks <nickson_yzj@hotmail.com>
2022-08-03 23:22:56 +0800

    subject class

---
+++ C206_CaseStudy/src/Subject.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+
+/**
+ * @author nicks
+ *
+ */
+public class Subject {
+	private String name;
+	private String description;
+	private String preRequisite;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getPreRequisite() {
+		return preRequisite;
+	}
+	public void setPreRequisite(String preRequisite) {
+		this.preRequisite = preRequisite;
+	}
+	@Override
+	public String toString() {
+		return "Subject [name=" + name + ", description=" + description + ", preRequisite=" + preRequisite + "]";
+	}
+	
+	
+}
