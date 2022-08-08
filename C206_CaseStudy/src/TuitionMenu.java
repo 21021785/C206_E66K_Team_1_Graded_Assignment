bcf6fc23897638c110bc10d42a55f525355322c2
AJ-VICTOR <21008455@myrp.edu.sg>
2022-08-08 09:26:34 +0800

    Tuition and Tuiotion menu

---
+++ C206_CaseStudy/src/TuitionMenu.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class TuitionMenu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayList<Tuition> tuitionList = new ArrayList<Tuition>();
+		
+	}
+	
+	
+
+}
